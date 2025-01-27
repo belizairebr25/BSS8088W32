d63137a69c49914c82b2010d3ec0daf5ffa5fb4c
belizairebr25 <65140356+belizairebr25@users.noreply.github.com>
2025-01-27 08:42:11 -0500

    Create clock.ino

---
+++ clock.ino
@@ -0,0 +1,32 @@
+#DEFINE clkp 23
+#DEFINE resp 22
+int delt = 10;
+
+
+void cpuStart(){
+  digitalWrite(resp, HIGH);
+  for(int i = 0; i < 4; i++)
+    cycle();    
+}
+
+void cycle(){
+  digitalWrite(clkp, HIGH);
+  delay(delt);
+  digitalWrite(clkp, LOW);
+  delay(delt);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(clkp, OUTPUT);
+  pinmode(resp, OUPUT);
+  Serial.println("Connected, Reseting CPU; Address == FFFF0");
+  cpuStart();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if()
+}
